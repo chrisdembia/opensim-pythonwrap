587112065114637aa6fcd31dddf3ab9004c37ca3
fca <fca@551e70df-b0f6-0310-80ee-9b1508f7454c>
2005-12-14 03:38:10 +0000

    Added a bases class for investigations.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@1374 551e70df-b0f6-0310-80ee-9b1508f7454c

---
+++ NMBLTK/Simulation/Model/Investigation.cpp
@@ -0,0 +1,195 @@
+// Investigation.cpp
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//=============================================================================
+// INCLUDES
+//=============================================================================
+#include "Investigation.h"
+
+using namespace std;
+
+
+//=============================================================================
+// CONSTRUCTOR(S) AND DESTRUCTOR
+//=============================================================================
+//_____________________________________________________________________________
+/**
+ * Destructor.
+ */
+Investigation::~Investigation()
+{
+}
+//_____________________________________________________________________________
+/**
+ * Default constructor.
+ */
+Investigation::Investigation():
+	_outputPrecision(_outputPrecisionProp.getValueInt())
+{
+	setType("Investigation");
+	setNull();
+}
+//_____________________________________________________________________________
+/**
+ * Construct from file.
+ *
+ * The object is constructed from the root element of the XML document.
+ * The type of object is the tag name of the XML root element.
+ *
+ * @param aFileName File name of the document.
+ */
+Investigation::Investigation(const string &aFileName):
+	rdObject(aFileName),
+	_outputPrecision(_outputPrecisionProp.getValueInt())
+{
+	setType("Investigation");
+	setNull();
+	updateFromXMLNode();
+}
+//_____________________________________________________________________________
+/**
+ * Construct from a DOMElement.
+ */
+Investigation::Investigation(DOMElement *aElement):
+	rdObject(aElement),
+	_outputPrecision(_outputPrecisionProp.getValueInt())
+{
+	setType("Investigation");
+	setNull();
+	updateFromXMLNode();
+}
+//_____________________________________________________________________________
+/**
+ * Copy constructor.
+ *
+ * Copy constructors for all Investigation's only copy the non-XML variable
+ * members of the object; that is, the object's DOMnode and rdXMLDocument
+ * are not copied but set to NULL.  The reason for this is that for the
+ * object and all its derived classes to establish the correct connection
+ * to the XML document nodes, the the object would need to reconstruct based
+ * on the XML document not the values of the object's member variables.
+ *
+ * There are three proper ways to generate an XML document for an Investigation:
+ *
+ * 1) Construction based on XML file (@see Investigation(const char *aFileName)).
+ * In this case, the XML document is created by parsing the XML file.
+ *
+ * 2) Construction by Investigation(const rdXMLDocument *aDocument).
+ * This constructor explictly requests construction based on an
+ * XML document.  In this way the proper connection between an object's node
+ * and the corresponding node within the XML document is established.
+ * This constructor is a copy constructor of sorts because all essential
+ * Investigation member variables should be held within the XML document.
+ * The advantage of this style of construction is that nodes
+ * within the XML document, such as comments that may not have any
+ * associated Investigation member variable, are preserved.
+ *
+ * 3) A call to generateDocument().
+ * This method generates an XML document for the Investigation from scratch.
+ * Only the essential document nodes are created (that is, nodes that
+ * correspond directly to member variables.).
+ *
+ * @param aInvestigation Object to be copied.
+ * @see Investigation(const rdXMLDocument *aDocument)
+ * @see Investigation(const char *aFileName)
+ * @see generateDocument()
+ */
+Investigation::Investigation(const Investigation &aInvestigation):
+	rdObject(aInvestigation),
+	_outputPrecision(_outputPrecisionProp.getValueInt())
+{
+	setNull();
+	*this = aInvestigation;
+}
+//_____________________________________________________________________________
+/**
+ * Set all member variables to their null or default values.
+ */
+void Investigation::
+setNull()
+{
+	setupProperties();
+	_model = 0;
+	_outputPrecision = 8;
+}
+//_____________________________________________________________________________
+/**
+ * Connect properties to local pointers.
+ */
+void Investigation::setupProperties()
+{
+	_outputPrecisionProp.setName("output_precision");
+	_propertySet.append( &_outputPrecisionProp );
+}
+
+
+
+//=============================================================================
+// OPERATORS
+//=============================================================================
+//_____________________________________________________________________________
+/**
+ * Assignment operator.
+ *
+ * @return Reference to this object.
+ */
+Investigation& Investigation::
+operator=(const Investigation &aInvestigation)
+{
+	// BASE CLASS
+	rdObject::operator=(aInvestigation);
+
+	// MEMEBER VARIABLES
+	_model = aInvestigation._model;
+
+	return(*this);
+}
+
+
+//=============================================================================
+// GET AND SET
+//=============================================================================
+//-----------------------------------------------------------------------------
+// MODEL
+//-----------------------------------------------------------------------------
+//_____________________________________________________________________________
+/**
+ * Set the model to be investigated.
+ */
+void Investigation::
+setModel(rdModel *aModel)
+{
+	_model = aModel;
+}
+//_____________________________________________________________________________
+/**
+ * Get the model to be investigated.
+ */
+rdModel* Investigation::
+getModel() const
+{
+	return(_model);
+}
+
+//-----------------------------------------------------------------------------
+// OUTPUT PRECISION
+//-----------------------------------------------------------------------------
+//_____________________________________________________________________________
+/**
+ * Set the output precision.
+ */
+void Investigation::
+setOutputPrecision(int aOutputPrecision)
+{
+	_outputPrecision = aOutputPrecision;
+}
+//_____________________________________________________________________________
+/**
+ * Get the output precision.
+ */
+int Investigation::
+getOutputPrecision() const
+{
+	return(_outputPrecision);
+}
---
+++ NMBLTK/Simulation/Model/Investigation.h
@@ -0,0 +1,85 @@
+// Investigation.h
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//#ifndef __Investigation_h__
+//#define __Investigation_h__
+
+#include <NMBLTK/Tools/rdObject.h>
+#include <NMBLTK/Tools/rdPropertyBool.h>
+#include <NMBLTK/Tools/rdPropertyStr.h>
+#include <NMBLTK/Tools/rdPropertyInt.h>
+#include <NMBLTK/Tools/rdArrayPtrs.h>
+#include <NMBLTK/Simulation/Model/rdModel.h>
+class rdXMLDocument;
+
+
+//=============================================================================
+//=============================================================================
+/**
+ * An abstract class for specifying the interface for an investigation.
+ *
+ * @author Frank C. Anderson
+ * @version 1.0
+ */
+class RDSIMULATION_API Investigation: public rdObject
+{
+//=============================================================================
+// DATA
+//=============================================================================
+protected:
+	/** Pointer to the model being investigated. */
+	rdModel *_model;
+
+	// PROPERTIES
+	/** Output precision. */
+	rdPropertyInt _outputPrecisionProp;
+	int &_outputPrecision;
+
+
+//=============================================================================
+// METHODS
+//=============================================================================
+	//--------------------------------------------------------------------------
+	// CONSTRUCTION
+	//--------------------------------------------------------------------------
+public:
+	Investigation();
+	Investigation(const std::string &aFileName);
+	Investigation(DOMElement *aElement);
+	virtual ~Investigation();
+	// Copy constrctor and virtual copy 
+	Investigation(const Investigation &aObject);
+private:
+	void setNull();
+	void setupProperties();
+
+	//--------------------------------------------------------------------------
+	// OPERATORS
+	//--------------------------------------------------------------------------
+public:
+#ifndef SWIG
+	Investigation& operator=(const Investigation &aInvestigation);
+#endif
+
+	//--------------------------------------------------------------------------
+	// GET AND SET
+	//--------------------------------------------------------------------------
+	virtual void setModel(rdModel *aModel);
+	virtual rdModel* getModel() const;
+	void setOutputPrecision(int aPrecision);
+	int getOutputPrecision() const;
+
+	//--------------------------------------------------------------------------
+	// INTERFACE
+	//--------------------------------------------------------------------------
+	virtual void run() = 0;
+	virtual void printResults(const char *aBaseName,const char *aDir=NULL,
+		double aDT=-1.0,const char *aExtension=".sto") = 0;
+
+//=============================================================================
+};	// END of class Investigation
+//=============================================================================
+//=============================================================================
+
+//#endif // __Investigation_h__
+
+
