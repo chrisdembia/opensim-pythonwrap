571efeedf92bf7b4e8f9691f6a00bd276a3ee67e
fca <fca@551e70df-b0f6-0310-80ee-9b1508f7454c>
2006-01-19 20:52:07 +0000

    Added a folder for applications.  The first application is a perturbation executable for running perturbation analyses on walking models.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@1391 551e70df-b0f6-0310-80ee-9b1508f7454c

---
+++ NMBLTK/Applications/Perturb/perturb.cpp
@@ -0,0 +1,68 @@
+// perturb.cpp
+// author:  Frank C. Anderson
+
+// INCLUDE
+#include <string>
+#include <iostream>
+#include <NMBLTK/Simulation/Model/rdModel.h>
+#include <NMBLTK/Simulation/Model/LoadModel.h>
+#include <NMBLTK/Analyses/InvestigationPerturbation.h>
+
+using namespace std;
+
+//_____________________________________________________________________________
+/**
+ * Main routine for conducting a perturbation analysis.
+ */
+int main(int argc,char **argv)
+{
+
+	// REGISTER TYPES
+	rdObject::RegisterType(InvestigationPerturbation());
+
+	// INTERPRET COMMAND LINE
+	string setupFileName = "";
+	string modelName = "";
+	if(argc!=3) {
+		cout<<"\n\nusage: perturb.exe setupFile.xml modelName\n\n";
+		exit(-1);
+	} else {
+		setupFileName = argv[1];
+		modelName = argv[2];
+	}
+
+	// CONSTRUCT INVESTIGATION
+	//InvestigationPerturbation perturb;
+	//perturb.print("defaultPertubation.xml");
+	cout<<"Loading perturbation setup file "<<setupFileName<<".\n\n";
+	InvestigationPerturbation perturb(setupFileName);
+	perturb.print("test2.xml");
+
+	// LOAD A MODEL
+	rdModel *model1 = LoadModel(modelName);
+	if(model1!=NULL) {
+		cout<<"-----------------------------------------------------------------------\n";
+		cout<<"Loaded library  "<<modelName<<"\n";
+		cout<<"-----------------------------------------------------------------------\n";
+		model1->printDetailedInfo(cout);
+		cout<<"-----------------------------------------------------------------------\n\n";
+	}
+
+	// LOAD ANOTHER MODEL
+	modelName = "cgmalWalking01_D";
+	rdModel *model2 = LoadModel(modelName);
+	if(model2!=NULL) {
+		cout<<"-----------------------------------------------------------------------\n";
+		cout<<"Loaded library  "<<modelName<<"\n";
+		cout<<"-----------------------------------------------------------------------\n";
+		model2->printBasicInfo(cout);
+		cout<<"-----------------------------------------------------------------------\n\n";
+	}
+
+	// RUN
+	cout<<"Running..."<<endl<<endl;
+	//perturb.setModel(model);
+	//perturb.run();
+
+	return(0);
+}
