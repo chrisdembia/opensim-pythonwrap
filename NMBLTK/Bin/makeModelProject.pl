b480f9cefb6a55231f4684549d40b1355718a8a4
fca <fca@551e70df-b0f6-0310-80ee-9b1508f7454c>
2006-01-24 19:53:05 +0000

    Added a new script written by Ayman for making a Visual Studio project for making a model dll.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@1404 551e70df-b0f6-0310-80ee-9b1508f7454c

---
+++ NMBLTK/Bin/makeModelProject.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl -w
+#
+
+use Cwd;
+$curFolder = cwd;
+
+# Parse curFolder to extract model name
+@dirArray = split(/\\|\//, $curFolder);
+$subjectName = $dirArray[$#dirArray];
+chomp($subjectName);
+
+use locale;
+
+print "Use subject name ".$subjectName." (y/n)?";
+$response = <STDIN>;
+if ($response =~ m/^[N|n]$/) {
+	print "Enter subject name :";
+	$subjectName = readline STDIN;
+	chomp($subjectName);	
+}
+
+my $CapSubjectName = "\u$subjectName";
+## my $suCapSubjectName = "su".$CapSubjectName;
+
+chomp($subjectName);
+%filemap = (
+	"template.sln" => $CapSubjectName.".sln",
+	"template.vcproj" => $CapSubjectName.".vcproj",
+	"template.h" => $CapSubjectName.".h",
+	"template.cpp" => $CapSubjectName.".cpp",
+	"sdufuncs_darryl.c" => "sdufuncs_darryl.c",
+);
+my $templateDir = $ENV{NMBL_INSTALL}.'/Template/';
+
+
+while ( my ($key, $value) = each(%filemap) ) {
+		my $templateFile = $templateDir.$key;
+		my $subjectFile = $value;
+	if(open(TEMPL_FH, "<$templateFile")) {
+		if(open(SUBJECT_FH, ">$subjectFile")) {
+		#Loop thru and substitute _template_ with $subjectName
+		while(<TEMPL_FH>){
+			$nextLine = $_;
+			$nextLine =~ s/_template_/$subjectName/g;
+			$nextLine =~ s/_Template_/$CapSubjectName/g;
+			print SUBJECT_FH $nextLine;
+		}
+		close(SUBJECT_FH);
+		print "Created file $subjectFile\n";
+		}
+		else {
+			die "Cannot open subject file";
+		}
+		close(SUBJECT_FH);
+	}
+	else {
+		die "Cannot find template file $templateFile";
+	}
+};
