f136d30ac2c8ab648293416e7a03cb6550c3fbb1
fca <fca@551e70df-b0f6-0310-80ee-9b1508f7454c>
2006-02-01 00:26:05 +0000

    Added the files for the RegisterType() method.  This method registers the types exported by the rdSimulation library.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@1417 551e70df-b0f6-0310-80ee-9b1508f7454c

---
+++ NMBLTK/Simulation/Model/RegisterTypes.cpp
@@ -0,0 +1,51 @@
+// RegisterTypes.cpp
+// author: Frank C. Anderson
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#include <string>
+#include <iostream>
+#include <NMBLTK/Tools/rdObject.h>
+#include "RegisterTypes.h"
+#include "rdActuatorSet.h"
+#include "rdContactForceSet.h"
+#include "rdForce.h"
+#include "rdTorque.h"
+#include "rdMuscle.h"
+#include "rdGeneralizedForce.h"
+#include <NMBLTK/Simulation/Manager/rdManager.h>
+#include <NMBLTK/Simulation/Control/rdControlConstant.h>
+#include <NMBLTK/Simulation/Control/rdControlLinear.h>
+
+using namespace std;
+
+
+//_____________________________________________________________________________
+/**
+ * The purpose of this routine is to register all class types exported by
+ * the rdSimulation library.
+ */
+RDSIMULATION_API void RegisterTypes()
+{
+	cout<<"rdSimulation.RegisterTypes:  \n";
+
+	//rdObject::RegisterType( rdManager() );
+
+	rdObject::RegisterType( rdControlSet() );
+	rdObject::RegisterType( rdControlConstant() );
+	rdObject::RegisterType( rdControlLinear() );
+	rdObject::RegisterType( rdControlLinearNode() );
+
+	rdObject::RegisterType( rdActuatorSet() );
+	rdObject::RegisterType( rdForce() );
+	rdObject::RegisterType( rdGeneralizedForce() );
+	rdObject::RegisterType( rdTorque() );
+	//rdObject::RegisterType( rdMuscle() );
+
+	rdObject::RegisterType( rdContactForceSet() );
+
+
+	//rdObject::RegisterType( rdIntegCallbackSet() );
+	//rdObject::RegisterType( rdDerivCallbackSet() );
+
+}
+
