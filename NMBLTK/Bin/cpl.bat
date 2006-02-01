5b2c60270bce12c7d0bf5aff21ad9cae52f8f621
fca <fca@551e70df-b0f6-0310-80ee-9b1508f7454c>
2006-02-01 00:21:41 +0000

    Added lines for copying executables to the LibDev directory in addition to the .lib, .dll, and .mll files.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@1416 551e70df-b0f6-0310-80ee-9b1508f7454c

--- NMBLTK/Bin/cpl.bat
+++ NMBLTK/Bin/cpl.bat
@@ -4,6 +4,7 @@ if x%1 == xD GOTO DEBUG
 for /r %%i in (Release\*.lib) do copy "%%i" "%RDI_DEV%\LibDev\%RDI_ARCH%"
 for /r %%i in (Release\*.dll) do copy "%%i" "%RDI_DEV%\LibDev\%RDI_ARCH%"
 for /r %%i in (Release\*.mll) do copy "%%i" "%RDI_DEV%\LibDev\%RDI_ARCH%"
+for /r %%i in (Release\*.exe) do copy "%%i" "%RDI_DEV%\LibDev\%RDI_ARCH%"
 GOTO END;
 
 :DEBUG
@@ -11,6 +12,7 @@ SET RDI_ARCHD=%RDI_ARCH%D
 for /r %%i in (Debug\*.lib) do copy "%%i" "%RDI_DEV%\LibDev\%RDI_ARCHD%"
 for /r %%i in (Debug\*.dll) do copy "%%i" "%RDI_DEV%\LibDev\%RDI_ARCHD%"
 for /r %%i in (Debug\*.mll) do copy "%%i" "%RDI_DEV%\LibDev\%RDI_ARCHD%"
+for /r %%i in (Debug\*.exe) do copy "%%i" "%RDI_DEV%\LibDev\%RDI_ARCHD%"
 
 :END
 exit(0)
