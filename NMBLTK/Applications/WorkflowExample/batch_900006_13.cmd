0182804eb9866c1b87d2524ea04a6e3c13bd8279
fca <fca@551e70df-b0f6-0310-80ee-9b1508f7454c>
2006-03-30 03:17:29 +0000

    Renamed the Darryl folder.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@1569 551e70df-b0f6-0310-80ee-9b1508f7454c

--- NMBLTK/Applications/WorkflowExample/batch_900006_13.cmd
+++
@@ -1,4 +0,0 @@
-scale gait23_gilletteControls scaleparams_gilControlsAllMarkers.txt data\900006 900006 900006_2 45.5 > data\900006\scale.log
-invkin invkinparams_gilControls.txt data\900006 900006 900006_13 900006_13ik > data\900006\invkin.log
-rea reaparams_gilControls.txt data\900006 900006 900006_13ik_lsw 900006_13rea_lsw > data\900006\rea.log
-cmc cmcparams_gilControls.txt gait92_corrected data\900006 900006 900006_13rea_lsw 900006_13cmc_lsw > data\900006\cmc.log
