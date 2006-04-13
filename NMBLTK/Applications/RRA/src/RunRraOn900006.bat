02317a18b8a88bc5c05fc6eb778d19f3574aa7fa
ctj <ctj@551e70df-b0f6-0310-80ee-9b1508f7454c>
2006-04-13 21:19:48 +0000

    Removed RRA source directory and example subject su900061 directory.  The RRA source directory will be moved into the RDI project in the form of an investigation, to work along with InvestigationCMC.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@1663 551e70df-b0f6-0310-80ee-9b1508f7454c

--- NMBLTK/Applications/RRA/src/RunRraOn900006.bat
+++
@@ -1,5 +0,0 @@
-pushd .
-cd ..\..\WorkflowExample
-..\RRA\src\rra\rra_project\Debug\rra_D.exe -ModelLibrary su900006_D.dll -Actuators su900006_rra.act -Params params.txt -Prefix 900006_13ik -Tracking su900006_rra.trk
-..\RRA\src\rra\rra_project\Debug\rra_D.exe -ModelLibrary su900006_D.dll -Actuators su900006_rra.act -Params params.txt -Prefix 900006_13ik_corrected_by_RRA -Tracking su900006_rra.trk -Correct no
-popd
--- NMBLTK/Applications/RRA/su900061/dynsim/src/README.txt
+++
@@ -1,2 +0,0 @@
-When SIMM creates the source files for a forward simulation,
-they are put in this folder.
