a97ee9834a9b9824243e2f1557b845e9a4f7bff1
aymanh <aymanh@551e70df-b0f6-0310-80ee-9b1508f7454c>
2006-06-13 23:02:18 +0000

    Initial version of gait workflow wizard spec.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@1829 551e70df-b0f6-0310-80ee-9b1508f7454c

---
+++ OpenSim/Applications/Gui/opensim/gait/src/org/opensim/gait/GaitWorkflowAction.java
@@ -0,0 +1,49 @@
+package org.opensim.gait;
+
+import java.awt.Dialog;
+import java.text.MessageFormat;
+import org.openide.DialogDisplayer;
+import org.openide.WizardDescriptor;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CallableSystemAction;
+
+public final class GaitWorkflowAction extends CallableSystemAction {
+    
+    public void performAction() {
+        // TODO implement action body
+        WizardDescriptor.Iterator iterator = new workflowWizardIterator();
+        WizardDescriptor wizardDescriptor = new WizardDescriptor(iterator);
+        // {0} will be replaced by WizardDescriptor.Panel.getComponent().getName()
+        // {1} will be replaced by WizardDescriptor.Iterator.name()
+        wizardDescriptor.setTitleFormat(new MessageFormat("{0} ({1})"));
+        wizardDescriptor.setTitle("Subject Specific Gait Workflow");
+        Dialog dialog = DialogDisplayer.getDefault().createDialog(wizardDescriptor);
+        dialog.setVisible(true);
+        dialog.toFront();
+        boolean cancelled = wizardDescriptor.getValue() != WizardDescriptor.FINISH_OPTION;
+        if (!cancelled) {
+            // do something
+        }
+
+    }
+    
+    public String getName() {
+        return NbBundle.getMessage(GaitWorkflowAction.class, "CTL_GaitWorkflowAction");
+    }
+    
+    protected void initialize() {
+        super.initialize();
+        // see org.openide.util.actions.SystemAction.iconResource() javadoc for more details
+        putValue("noIconInMenu", Boolean.TRUE);
+    }
+    
+    public HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+    
+    protected boolean asynchronous() {
+        return false;
+    }
+    
+}
