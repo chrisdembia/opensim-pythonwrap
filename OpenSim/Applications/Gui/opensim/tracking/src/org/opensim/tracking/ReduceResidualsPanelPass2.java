1e072c2af5ffe717c30cc2d9f82374a5468bb77b
aymanh <aymanh@551e70df-b0f6-0310-80ee-9b1508f7454c>
2006-10-08 20:54:16 +0000

    Commit new files for GUI missed by last commit for separate RRA pass1, pass2
    git-svn-id: https://simtk.org/svn/opensim/Trunk@2053 551e70df-b0f6-0310-80ee-9b1508f7454c

---
+++ OpenSim/Applications/Gui/opensim/tracking/src/org/opensim/tracking/ReduceResidualsPanelPass2.java
@@ -0,0 +1,91 @@
+package org.opensim.tracking;
+
+import com.realisticDynamics.InvestigationRRA;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+import org.netbeans.api.progress.ProgressHandle;
+import org.netbeans.api.progress.ProgressHandleFactory;
+import org.openide.util.HelpCtx;
+import org.opensim.modeling.Model;
+
+public class ReduceResidualsPanelPass2  extends workflowWizardPanelBase{
+    
+    /**
+     * The visual component that displays this panel. If you need to access the
+     * component from this class, just use getComponent().
+     */
+    private ReduceResidualsVisualPanelPass2 component;
+    
+    // Get the visual component for the panel. In this template, the component
+    // is kept separate. This can be more efficient: if the wizard is created
+    // but never displayed, or not all panels are displayed, it is better to
+    // create only those which really need to be visible.
+    public Component getComponent() {
+        if (component == null) {
+            component = new ReduceResidualsVisualPanelPass2(this);
+        }
+        return component;
+    }
+    
+    public HelpCtx getHelp() {
+        // Show no Help button for this panel:
+        return HelpCtx.DEFAULT_HELP;
+        // If you have context help:
+        // return new HelpCtx(SampleWizardPanel1.class);
+    }
+    
+    /*
+    private final Set<ChangeListener> listeners = new HashSet<ChangeListener>(1);
+    public final void addChangeListener(ChangeListener l) {
+        synchronized (listeners) {
+            listeners.add(l);
+        }
+    }
+    public final void removeChangeListener(ChangeListener l) {
+        synchronized (listeners) {
+            listeners.remove(l);
+        }
+    }
+    protected final void fireChangeEvent() {
+        Iterator<ChangeListener> it;
+        synchronized (listeners) {
+            it = new HashSet<ChangeListener>(listeners).iterator();
+        }
+        ChangeEvent ev = new ChangeEvent(this);
+        while (it.hasNext()) {
+            it.next().stateChanged(ev);
+        }
+    }
+     */
+    
+    // You can use a settings object to keep track of state. Normally the
+    // settings object will be the WizardDescriptor, so you can use
+    // WizardDescriptor.getProperty & putProperty to store information entered
+    // by the user.
+    public void readSettings(Object settings) {
+        descriptor = (WorkflowDescriptor) settings;
+        component.updatePanel(descriptor);
+        updateVisibility();
+    }
+    public void storeSettings(Object settings) {
+       descriptor = (WorkflowDescriptor) settings;
+       component.updateWorkflow(descriptor);
+    }
+
+    boolean executeStep() {
+        
+       component.updateWorkflow(descriptor);
+        final InvestigationRRA rra = new InvestigationRRA(descriptor.getSetupRRA_pass2Filename());
+        runDynamicInvestigation(rra,false);
+          return true;
+    }
+    
+    public void updateVisibility()
+    {
+        markValid(!descriptor.getStepInProgress());
+    }
+   
+}
+
