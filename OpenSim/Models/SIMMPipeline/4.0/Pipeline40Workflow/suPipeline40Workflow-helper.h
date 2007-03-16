caa7becd1d602e7b451d16d972a11a2d97f07df2
erang <erang@551e70df-b0f6-0310-80ee-9b1508f7454c>
2007-03-16 23:31:54 +0000

    Removed
    git-svn-id: https://simtk.org/svn/opensim/Trunk@2779 551e70df-b0f6-0310-80ee-9b1508f7454c

--- OpenSim/Models/SIMMPipeline/4.0/Pipeline40Workflow/suPipeline40Workflow-helper.h
+++
@@ -1,7 +0,0 @@
-
-// Clay Anderson added the following two lines to integrate Darryl Thelen's changes
-// to the pipeline.
-struct DP_Data;
-extern "C" void init_gaitsim(dpModelStruct* sdm, MotionData* data,double t,double *y);
-extern "C" void DP_MainStartup_Workflow(DP_Data *dpd,const char *workpath,const char *paramfile);
-extern "C" void computeConstrainedCoords(dpModelStruct* sdm,double* y);
