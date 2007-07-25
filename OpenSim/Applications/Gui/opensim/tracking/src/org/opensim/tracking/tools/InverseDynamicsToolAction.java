85209b0eba89f764dca73603a916e6492690ffdb
erang <erang@551e70df-b0f6-0310-80ee-9b1508f7454c>
2007-07-25 04:11:47 +0000

    initial work on analyze tool panel (will use this as the inverse dynamics panel too)
    git-svn-id: https://simtk.org/svn/opensim/Trunk@3397 551e70df-b0f6-0310-80ee-9b1508f7454c

---
+++ OpenSim/Applications/Gui/opensim/tracking/src/org/opensim/tracking/tools/InverseDynamicsToolAction.java
@@ -0,0 +1,48 @@
+package org.opensim.tracking.tools;
+
+import java.io.IOException;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CallableSystemAction;
+import org.opensim.modeling.Model;
+import org.opensim.tracking.AnalyzeToolPanel;
+import org.opensim.tracking.BaseToolPanel;
+import org.opensim.utils.ErrorDialog;
+import org.opensim.view.pub.OpenSimDB;
+
+public final class InverseDynamicsToolAction extends CallableSystemAction {
+   
+   public void performAction() {
+      Model model = OpenSimDB.getInstance().getCurrentModel();
+      if(model==null) return;
+
+      try {
+         final AnalyzeToolPanel panel = AnalyzeToolPanel.createInverseDynamicsPanel(model);
+         BaseToolPanel.openToolDialog(panel, "Analyze Tool");
+      } catch (IOException ex) {
+         ErrorDialog.displayIOExceptionDialog("Unexpected error","Unexpected error while initializing forward tool",ex);
+      }
+   }
+   
+   public String getName() {
+      return NbBundle.getMessage(InverseDynamicsToolAction.class, "CTL_InverseDynamicsToolAction");
+   }
+   
+   protected void initialize() {
+      super.initialize();
+      // see org.openide.util.actions.SystemAction.iconResource() javadoc for more details
+      putValue("noIconInMenu", Boolean.TRUE);
+   }
+   
+   public HelpCtx getHelpCtx() {
+      return HelpCtx.DEFAULT_HELP;
+   }
+   
+   protected boolean asynchronous() {
+      return false;
+   }
+   
+   public boolean isEnabled() {
+      return OpenSimDB.getInstance().getCurrentModel()!=null;
+   }
+}
