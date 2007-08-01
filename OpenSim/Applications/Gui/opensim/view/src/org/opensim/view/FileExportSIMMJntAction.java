08e8cf8f032acdbcc6455f759cac8b8f94b45ef6
aymanh <aymanh@551e70df-b0f6-0310-80ee-9b1508f7454c>
2007-08-01 17:20:16 +0000

    Modify Export to Simm command to query for both jnt and msl file names.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@3479 551e70df-b0f6-0310-80ee-9b1508f7454c

--- OpenSim/Applications/Gui/opensim/view/src/org/opensim/view/FileExportSIMMJntAction.java
+++ OpenSim/Applications/Gui/opensim/view/src/org/opensim/view/FileExportSIMMJntAction.java
@@ -1,12 +1,18 @@
 package org.opensim.view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
 import org.openide.awt.StatusDisplayer;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
 import org.opensim.modeling.Model;
 import org.opensim.modeling.SimmFileWriter;
-import org.opensim.utils.FileUtils;
+import org.opensim.view.actions.OpenSimToSIMMOptionsJPanel;
 import org.opensim.view.pub.OpenSimDB;
 import org.opensim.view.pub.ViewDB;
 
@@ -16,20 +22,55 @@ import org.opensim.view.pub.ViewDB;
  */
 public final class FileExportSIMMJntAction extends CallableSystemAction {
     
+    class ExportDlgListener implements ActionListener, PropertyChangeListener
+    {
+        DialogDescriptor dlgOfInterest;
+        ExportDlgListener(DialogDescriptor aDialog)
+        {
+            super();
+            dlgOfInterest=aDialog;
+            dlgOfInterest.addPropertyChangeListener(this);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            dlgOfInterest.setValid(!dlgOfInterest.isValid());
+        }
+
+        public void propertyChange(PropertyChangeEvent evt) {
+            String name=evt.getPropertyName();
+            int i=0;
+        }
+    }
     public void performAction() {
         // TODO implement action body
         Model mdl = OpenSimDB.getInstance().getCurrentModel();
         if (mdl != null){
-            
-            String jntFileName = FileUtils.getInstance().browseForFilename(".jnt", "SIMM .jnt file", false);
-            if (jntFileName!=null){
+            OpenSimToSIMMOptionsJPanel exportPanel = new OpenSimToSIMMOptionsJPanel();
+            DialogDescriptor dlg = new DialogDescriptor(exportPanel, "Export SIMM Model");
+            dlg.setValid(false);
+            dlg.setButtonListener(new ExportDlgListener(dlg));
+            DialogDisplayer.getDefault().createDialog(dlg).setVisible(true);
+            Object userInput = dlg.getValue();
+            if (((Integer)userInput).compareTo((Integer)DialogDescriptor.OK_OPTION)==0){
+                String jntfileName = exportPanel.getJointFilename();
+                if (jntfileName==null)
+                    return;
                 // Make sure we have the right extension.
-                if (!jntFileName.endsWith(".jnt"))
-                    jntFileName = jntFileName+".jnt";
-                // Create a SimmFileWriter for the model and invoke its writeJointFile
-                new SimmFileWriter(mdl).writeJointFile(jntFileName);
-                StatusDisplayer.getDefault().setStatusText("Exported SIMM jnt file for model "+
-                        mdl.getName()+" to file "+jntFileName+".");
+                if (!jntfileName.endsWith(".jnt"))
+                    jntfileName = jntfileName+".jnt";
+                SimmFileWriter modelWriter=new SimmFileWriter(mdl);
+                modelWriter.writeJointFile(jntfileName);
+
+                // Now the muscles
+                String mslfileName = exportPanel.getMslFilename();
+                if (mslfileName==null || mslfileName.equalsIgnoreCase(""))
+                    return;
+                // Make sure we have the right extension.
+                if (!mslfileName.endsWith(".msl"))
+                    mslfileName = mslfileName+".msl";
+                modelWriter.writeMuscleFile(mslfileName);
+                StatusDisplayer.getDefault().setStatusText("Exported SIMM jnt & muscle files for model "+
+                        mdl.getName()+".");
             }
         }
     }
