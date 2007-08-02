45bf317b6e29ec1c5aa4c813bacb1ea2b94f2135
aymanh <aymanh@551e70df-b0f6-0310-80ee-9b1508f7454c>
2007-08-02 07:08:58 +0000

    git-svn-id: https://simtk.org/svn/opensim/Trunk@3501 551e70df-b0f6-0310-80ee-9b1508f7454c

--- OpenSim/Applications/Gui/opensim/view/src/org/opensim/view/nodes/ModelInfoAction.java
+++ OpenSim/Applications/Gui/opensim/view/src/org/opensim/view/nodes/ModelInfoAction.java
@@ -1,30 +1,35 @@
 package org.opensim.view.nodes;
 
+import javax.swing.JButton;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
 import org.openide.nodes.Node;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
-import org.opensim.utils.TheApp;
 import org.opensim.view.ExplorerTopComponent;
-import org.opensim.view.nodes.ConcreteModelNode;
-import org.opensim.view.pub.ViewDB;
 import org.opensim.modeling.Model;
+import org.opensim.view.ModelInfoJPanel;
 
 
 public final class ModelInfoAction extends CallableSystemAction {
     
     public void performAction() {
-        ModelInfoDialog dlg=new ModelInfoDialog(TheApp.getAppFrame(), true);
+        ModelInfoJPanel infoPanel=new ModelInfoJPanel();
         Node[] selected = ExplorerTopComponent.findInstance().getExplorerManager().getSelectedNodes();
         // Action shouldn't be available otherwise'
         ConcreteModelNode modelNode = (ConcreteModelNode) selected[0];
         Model mdl = modelNode.getModel();
-        dlg.setModelName(mdl.getName());
-        dlg.setModelFile(mdl.getInputFileName());
-        dlg.setAuthors(
-                "Model developed by..");
-        dlg.setReferences("Reference:IEEE TBME, vol. vv, pp. 1-2, 2000.");
-        dlg.setVisible(true);
+        infoPanel.setModelName(mdl.getName());
+        infoPanel.setModelFile(mdl.getInputFileName());
+        infoPanel.setDynamicsEngineName(mdl.getDynamicsEngine().getType());
+        infoPanel.setAuthors(
+                mdl.getCredits());
+        infoPanel.setReferences(mdl.getPublications());
+        DialogDescriptor dlg = new DialogDescriptor(infoPanel, "Model Info.");
+        dlg.setOptions(new Object[]{new JButton("Close")});
+        dlg.setClosingOptions(null);
+        DialogDisplayer.getDefault().notify(dlg);
     }
     
     public String getName() {
