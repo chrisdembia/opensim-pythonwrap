6d1d4bc7f40533c81d802485febaf4e31e99b4c3
aymanh <aymanh@551e70df-b0f6-0310-80ee-9b1508f7454c>
2007-09-12 16:37:27 +0000

    Fix bug 428 (generic Java icon on file browsers)
    git-svn-id: https://simtk.org/svn/opensim/Trunk@3788 551e70df-b0f6-0310-80ee-9b1508f7454c

--- OpenSim/Applications/Gui/opensim/utils/src/org/opensim/utils/Installer.java
+++ OpenSim/Applications/Gui/opensim/utils/src/org/opensim/utils/Installer.java
@@ -1,6 +1,9 @@
 package org.opensim.utils;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 import org.openide.modules.ModuleInstall;
+import org.openide.windows.WindowManager;
 
 /**
  * Manages a module's lifecycle. Remember that an installer is optional and
@@ -12,6 +15,12 @@ public class Installer extends ModuleInstall {
         // By default, do nothing.
         // Put your startup code here.
         super.restored();
+        SwingUtilities.invokeLater(new Runnable() {
+
+         public void run() {
+            TheApp.setAppFrame((JFrame)WindowManager.getDefault().getMainWindow());
+         }
+      });
     }
     
 }
