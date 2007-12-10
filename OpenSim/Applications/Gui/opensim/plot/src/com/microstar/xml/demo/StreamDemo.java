557aec8d803a379adbdd8d481613e48951893621
aymanh <aymanh@551e70df-b0f6-0310-80ee-9b1508f7454c>
2007-12-10 23:36:09 +0000

    Update OpenSim source copyright notice.
    git-svn-id: https://simtk.org/svn/opensim/Trunk@3915 551e70df-b0f6-0310-80ee-9b1508f7454c

--- OpenSim/Applications/Gui/opensim/plot/src/com/microstar/xml/demo/StreamDemo.java
+++ OpenSim/Applications/Gui/opensim/plot/src/com/microstar/xml/demo/StreamDemo.java
@@ -13,14 +13,29 @@ import com.microstar.xml.XmlParser;
 /**
   * Demonstration application showing &AElig;fred's event stream from a stream.
   * <p>Usage: <code>java StreamDemo</code>
-  * @author Copyright (c) 1998 by Microstar Software Ltd.;
-  * @author written by David Megginson &lt;dmeggins@microstar.com&gt;
-  * @version 1.1
-  * @since Ptolemy II 0.2
-  * @see com.microstar.xml.XmlParser
-  * @see com.microstar.xml.XmlHandler
-  * @see XmlApp
-  * @see EventDemo
+  * Copyright (c)  1998 by Microstar Software Ltd.;
+* Use of the OpenSim software in source form is permitted provided that the following
+* conditions are met:
+* 	1. The software is used only for non-commercial research and education. It may not
+*     be used in relation to any commercial activity.
+* 	2. The software is not distributed or redistributed.  Software distribution is allowed 
+*     only through https://simtk.org/home/opensim.
+* 	3. Use of the OpenSim software or derivatives must be acknowledged in all publications,
+*      presentations, or documents describing work in which OpenSim or derivatives are used.
+* 	4. Credits to developers may not be removed from executables
+*     created from modifications of the source.
+* 	5. Modifications of source code must retain the above copyright notice, this list of
+*     conditions and the following disclaimer. 
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+*  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+*  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+*  SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+*  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+*  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+*  OR BUSINESS INTERRUPTION) OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+*  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   */
 public class StreamDemo extends EventDemo {
     public static void main(String[] args) throws Exception {
--- OpenSim/Applications/Gui/opensim/view/src/org/opensim/view/editors/TreeTableModel.java
+++ OpenSim/Applications/Gui/opensim/view/src/org/opensim/view/editors/TreeTableModel.java
@@ -2,21 +2,29 @@ package org.opensim.view.editors;
 /*
  * TreeTableModel.java
  *
- * Copyright (c) 1998 Sun Microsystems, Inc. All Rights Reserved.
- *
- * This software is the confidential and proprietary information of Sun
- * Microsystems, Inc. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Sun.
- *
- * SUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
- * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT. SUN SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * THIS SOFTWARE OR ITS DERIVATIVES.
- *
+ * Copyright (c)  1998 Sun Microsystems, Inc. All Rights Reserved.
+* Use of the OpenSim software in source form is permitted provided that the following
+* conditions are met:
+* 	1. The software is used only for non-commercial research and education. It may not
+*     be used in relation to any commercial activity.
+* 	2. The software is not distributed or redistributed.  Software distribution is allowed 
+*     only through https://simtk.org/home/opensim.
+* 	3. Use of the OpenSim software or derivatives must be acknowledged in all publications,
+*      presentations, or documents describing work in which OpenSim or derivatives are used.
+* 	4. Credits to developers may not be removed from executables
+*     created from modifications of the source.
+* 	5. Modifications of source code must retain the above copyright notice, this list of
+*     conditions and the following disclaimer. 
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+*  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+*  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+*  SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+*  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+*  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+*  OR BUSINESS INTERRUPTION) OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+*  WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 import javax.swing.tree.TreeModel;
