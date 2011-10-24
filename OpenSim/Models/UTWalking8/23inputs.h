77e6b29591a4bb351e4c3330191ffba9d3c07a34
aymanh <aymanh@551e70df-b0f6-0310-80ee-9b1508f7454c>
2011-10-24 19:40:33 +0000

    Remove obsolete files
    git-svn-id: https://simtk.org/svn/opensim/Trunk@6671 551e70df-b0f6-0310-80ee-9b1508f7454c

--- OpenSim/Models/UTWalking8/23inputs.h
+++
@@ -1,5 +0,0 @@
-#ifndef _23inputs_h_
-#define _23inputs_h_
-int input23s_(char *file,int *n,double t[],double q[][nq]);
-int input23a_(int *n,double t[],double q[][nq]);
-int mirror_(double qo[nq],double qn[nq]);
